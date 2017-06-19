739e599cbd5942af7a1db0c2c271dcffd155fb20
Rob van Daal <rvandaal@hotmail.com>
2017-06-19 16:37:49 +0200

    server-setup

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var mongoose = require('mongoose');
+var config = require('./config/database');
+var path = require('path');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, (err) => {
+	if(err) {
+		console.log('Could not connect to database: ', err);
+	} else {
+		console.log('Connected to database: ' + config.db);
+	}
+});
+
+app.use(express.static(__dirname + '/client/dist/'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+  console.log('dir: ' + __dirname + '/client/dist/index.html');
+});
+
+app.listen(8080, () => {
+	console.log('Listening on port 8080');
+});
